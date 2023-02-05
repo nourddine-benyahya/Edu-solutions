c7112ec8c24759e9b279149984c63a426f77f8b0
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2023-02-05 13:41:26 +0100

    lastCommitLocalStorage

--- client/pages/index.js
+++ client/pages/index.js
@@ -134,7 +134,7 @@ const HomePage = () => {
         <div className={styles.subtitle}>Join The Community of Questioners and Knowledgable Ones: Where Knowledge Meets Conversation</div>
         <div className={styles.text}>the ultimate platform for all your questions and answers. Here, you can connect with a community of experts and enthusiasts in various subjects such as programming, math, physics, English, medicine, and many more. Whether you have a question or a wealth of knowledge to share, join us and be part of the conversation. Let's build a world of knowledge, together
         </div>
-        <button className={styles.homeBtn} onClick={(e)=>(setpageNr(true))} >Let’s Started</button>
+        <button className={styles.homeBtn} value={true} onClick={handleValueChange} >Let’s Started</button>
       </div>
       <div>
       <img src="https://img.freepik.com/free-vector/learning-concept-illustration_114360-6186.jpg?w=740&t=st=1675593625~exp=1675594225~hmac=12c817c8dbb687e77f5d86557d35e5ae0b515335f3323f1d6e7f3446682936d4" alt="" className={styles.image} />
