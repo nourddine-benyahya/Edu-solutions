699940cca33340bdfda3fb69d1ebc821af0b5121
imorabet <mrabetikhlas700@gmail.com>
2023-02-05 12:42:01 +0100

    config file

--- client/site.config.js
+++ client/site.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
   title: 'Edu-Solutions',
   googleAnalytic: 'UA-149699646-1',
-=======
-  title: 'EduSolutions',
-  description: 'Where Developers Learn & Share',
-  siteUrl: 'https://clone-of-stackoverflow.vercel.app',
-  googleAnalytic: 'UA-149699646-1',
-  author: {
-    email: 'salihozdemir94@gmail.com',
-    name: 'One4All'
-  },
->>>>>>> 74aa133937cb47e89da598e584d122fbbb564b82
   lang: 'en'
 }
